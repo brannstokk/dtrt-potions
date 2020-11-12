e36ad908eb4b411eb5b9913f534c740d9df45b58
jlucas <jlucas@pachira.106.net>
2020-11-12 16:07:35 -0500

    Handle bandages as well; and other changes

--- DTRTPotions.lua
+++ DTRTPotions.lua
@@ -3,6 +3,10 @@
 	License: MIT License
 ]]
 
+dtrtHealingItem = nil
+dtrtManaItem = nil
+dtrtBandageItem = nil
+
 local PotFrame = CreateFrame("Frame")
 PotFrame:RegisterEvent("BAG_UPDATE")
 PotFrame:RegisterEvent("PLAYER_ENTERING_WORLD")
@@ -11,15 +15,20 @@ PotFrame:SetScript("OnEvent", function(...)
 
 if InCombatLockdown() then return end
 
--- local healthstone = 19012 -- Major Healthstone
--- local normalPot = 13446 -- Major Healing Potion
--- local pvpPot = 18839 -- Combat Healing Potion
 local name, type = GetInstanceInfo()
+local zone = GetZoneText()
 local macro = "#showtooltip\n/use "
 local healingMacroName = "Heal"
-local healingItemName = "";
+local healingItemName = ""
 local manaMacroName = "Mana"
-local manaItemName= "";
+local manaItemName= ""
+local bandageMacroName = "Bandage"
+local bandageItemName = ""
+
+local alteracBandageId = 19307    -- AV vendor bandage
+local highlanderBandageId = 20243 -- AB vendor bandage
+local arathiBandageId = 20066     -- AB quest care package bandage
+local wsgBandageId = 19066        -- WSG vendor bandage
 
 -- Healing item lists
 
@@ -53,12 +62,18 @@ local pvpManaItems = {
    13443, -- Superior Mana Potion (https://classic.wowhead.com/item=13443)
 }
 
-if GetItemCount(healthstone) ~= 0 then
+local bandageItems = {
+   14530, -- Heavy Runecloth Bandage (https://classic.wowhead.com/item=14530)
+}
+
+-- Set healing item
+
+if GetItemCount(healthstone) > 0 then
    healingItemName = select(1, GetItemInfo(healthstone)) -- use healthstone
 elseif type == "pvp" then
    for idx, item in ipairs(pvpHealthItems) do
       -- print("Checking for "..item)
-      if GetItemCount(item) ~= 0 then
+      if GetItemCount(item) > 0 then
          -- print("Item count of "..item.." is"..GetItemCount(item))
          healingItemName = select(1, GetItemInfo(item)) -- use pvp pot
          break
@@ -66,17 +81,19 @@ elseif type == "pvp" then
    end
 else
    for idx, item in ipairs(normalHealthItems) do
-      if GetItemCount(item) ~= 0 then
+      if GetItemCount(item) > 0 then
          healingItemName = select(1, GetItemInfo(item)) -- use normal pot
          break
       end
    end
 end
 
+-- Set mana potion item
+
 if type == "pvp" then
    for idx, item in ipairs(pvpManaItems) do
       -- print("Checking for "..item)
-      if GetItemCount(item) ~= 0 then
+      if GetItemCount(item) > 0 then
          -- print("Item count of "..item.." is"..GetItemCount(item))
          manaItemName = select(1, GetItemInfo(item)) -- use pvp pot
          break
@@ -84,25 +101,77 @@ if type == "pvp" then
    end
 else
    for idx, item in ipairs(normalManaItems) do
-      if GetItemCount(item) ~= 0 then
+      if GetItemCount(item) > 0 then
          manaItemName = select(1, GetItemInfo(item)) -- use normal pot
          break
       end
    end
 end
 
+-- Set bandage item
+
+if type == "pvp" then
+   -- For some reason, GetZoneText() still reports "Stormwind City"
+   -- when first zoning into battlegrounds
+
+   -- i = 0
+   -- while GetZoneText() == "Stormwind City" do
+   --    i = i + 1
+   --    print("Waiting for zone update "..i)
+   --    if i == 100 then
+   --       print("Breaking wait loop...")
+   --       break
+   --    end
+   -- end
+   if zone == "Alterac Valley" and GetItemCount(alteracBandageId) > 0 then
+      bandageItemName = select(1, GetItemInfo(alteracBandageId))
+   elseif zone == "Arathi Basin" and GetItemCount(arathiBandageId) > 0 then
+      bandageItemName = select(1, GetItemInfo(arathiBandageId))
+   elseif zone == "Arathi Basin" and GetItemCount(highlanderBandageId) > 0 then
+      bandageItemName = select(1, GetItemInfo(highlanderBandageId))
+   elseif zone == "Warsong Gulch" and GetItemCount(wsgBandageId) > 0 then
+      bandageItemName = select(1, GetItemInfo(wsgBandageId))
+   -- XXX duplicate code
+   else
+      for idx, item in ipairs(bandageItems) do
+         if GetItemCount(item) > 0 then
+            bandageItemName = select(1, GetItemInfo(item))
+         end
+      end
+   end
+else
+   -- XXX duplicate code
+   for idx, item in ipairs(bandageItems) do
+      if GetItemCount(item) > 0 then
+         bandageItemName = select(1, GetItemInfo(item))
+      end
+   end
+end
+
+-- Build macros
+
 if healingItemName ~= "" then
    EditMacro(healingMacroName,healingMacroName,nil,macro..healingItemName)
-   print("Macro "..healingMacroName.." is now using "..healingItemName)
-else
-   print("Macro "..healingMacroName.." was not modified: No healing items were found")
+   if healingItemName ~= dtrtHealingItem then
+      dtrtHealingItem = healingItemName
+      print("Macro "..healingMacroName.." is now using "..healingItemName)
+   end
 end
 
 if manaItemName ~= "" then
    EditMacro(manaMacroName,manaMacroName,nil,macro..manaItemName)
-   print("Macro "..manaMacroName.." is now using "..manaItemName)
-else
-   print("Macro "..manaMacroName.." was not modified: No healing items were found")
+   if manaItemName ~= dtrtManaItem then
+      dtrtManaItem = manaItemName
+      print("Macro "..manaMacroName.." is now using "..manaItemName)
+   end
+end
+
+if bandageItemName ~= "" then
+      EditMacro(bandageMacroName,bandageMacroName,nil,macro..bandageItemName)
+   if bandageItemName ~= dtrtBandageItem then
+      dtrtBandageItem = bandageItemName
+      print("Macro "..bandageMacroName.." is now using "..bandageItemName)
+   end
 end
 
 end)
